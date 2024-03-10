1b9cbb315fdf70922a16384387af09ab33555a8d
Pg1606 <pg16061999@gmail.com>
2024-03-10 16:45:28 +0530

    updating our directory page with directory component and category item component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import CategoryItem from './components/category-item/category-item.component.jsx';
-import './categories.styles.scss';
-import { categories } from './components/directory/directory.component.jsx';
+import Directory from './components/directory/directory.component';
+
+const categories = [
+  {
+    "id": 1,
+    "title": "hats",
+    "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+  },
+  {
+    "id": 2,
+    "title": "jackets",
+    "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+  },
+  {
+    "id": 3,
+    "title": "sneakers",
+    "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+  },
+  {
+    "id": 4,
+    "title": "womens",
+    "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+  },
+  {
+    "id": 5,
+    "title": "mens",
+    "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+  }
+];
 
 const App = () => {
   return (
-    <div className="categories-container">
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
-      ))};
-    </div>
+    <Directory categories={categories} />
   );
 };
 
