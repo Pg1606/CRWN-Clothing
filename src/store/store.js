589157613fdf98b31c61716cb1193fe2646b1e3b
Pg1606 <pg16061999@gmail.com>
2024-05-05 12:34:34 +0530

    moving our logger into middleware folder, adding required config for redux devtools

--- src/store/store.js
+++ src/store/store.js
@@ -1,25 +1,25 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-//import logger from "redux-logger";
+import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
 
 //root-reducer
 
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
+// const loggerMiddleware = (store) => (next) => (action) => {
+//   if (!action.type) {
+//     return next(action);
+//   }
 
-  console.log('type: ', action.type);
-  console.log('payload: ', action.payload);
-  console.log('currentState: ', store.getState());
+//   console.log('type: ', action.type);
+//   console.log('payload: ', action.payload);
+//   console.log('currentState: ', store.getState());
 
-  next(action);
+//   next(action);
 
-  console.log('next state: ', store.getState());
-};
+//   console.log('next state: ', store.getState());
+// };
 
 const persistConfig = {
   key: 'root',
@@ -29,9 +29,12 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [loggerMiddleware];
+//const middleWares = [logger];//[loggerMiddleware]
+const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean); //'production', 'development'
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(persistedReducer, undefined, composedEnhancers);
 
