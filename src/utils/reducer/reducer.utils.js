81b74dba17fa2ecf2a6a0a78af92c00ff36dd278
Pg1606 <pg16061999@gmail.com>
2024-05-18 17:26:02 +0530

    install Typescript libraries

--- src/utils/reducer/reducer.utils.js
+++ src/utils/reducer/reducer.utils.js
@@ -1,1 +1,12 @@
+// import { AnyAction } from "redux-saga";
+
+// export type ActionWithPayload<T, P> = {
+//   type: T;
+//   payload: P;
+// }
+
+// export type Action<T> = {
+//   type: T;
+// }
+
 export const createAction = (type, payload) => ({ type, payload });
